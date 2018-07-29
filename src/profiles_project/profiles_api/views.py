9e2e493cc635bb1cd4e9a94af799b6e5c12b23e5
Prithvi45 <maneprithviraj@sggs.ac.in>
2018-07-30 02:24:38 +0530

    created APIView and Tested

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import  serializers
 
 # Create your views here.
 
 class HelloAPIView(APIView):
     """ Test API View  """
 
+    serializer_class = serializers.HelloSerializers
     def get(self, request, format=None):
         """ Returns a list of APIView features"""
 
@@ -18,3 +21,30 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message':'Hello!','api_view':an_apiview})
+
+    def post(self, request):
+        """ create a hello msg with our name """
+        serializer = serializers.HelloSerializers(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message, 'name':name})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """ Handles updating an object """
+
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        """ Patch request, only updates fields provided in the request"""
+
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        """ Deletes objects """
+
+        return Response({'method':'delete'})
